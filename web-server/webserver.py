409441a3b74fc71b6fde323f96b5e14fb8ad95b3
Jotron <jotron@protonmail.com>
2019-01-17 17:51:41 +0100

    fix(webserver): round predictions
    - round webserver predictions
    - webserver uses model_ens.h5 model
    - comment webserver
    - add webserver tutorial
    - fix Pipfile

--- web-server/webserver.py
+++ web-server/webserver.py
@@ -14,35 +14,43 @@ import tensorflow as tf
 import numpy as np
 
 # retrieve trained model
-model = load_model('models/berlin_net/berlin_net.h5', custom_objects={'Melspectrogram':kapre.time_frequency.Melspectrogram}) #compile=false
-graph = tf.get_default_graph()
+model = load_model('models/model_ens.h5', custom_objects={'Melspectrogram':kapre.time_frequency.Melspectrogram,'Normalization2D':kapre.utils.Normalization2D})
 
+# initialize tensorflow graph
+graph = tf.get_default_graph()
 
 # initialize
 app = Flask(__name__)
 
-# prediction
+# make prediction
 def predict(tmp_file):
+    # read virtual file
     signal, sr = sf.read(tmp_file, dtype='float64')
 
-    # not 16k sampleRate (safari)
+    # if not 16k sampleRate (e.g. safari) -> resample
     if (sr != 16000):
         signal = librosa.resample(signal, sr, 16000)
         sr = 16000
 
-    # recording is too short (failing frontend)
+    # if recording is too short (e.g. failing frontend) -> print error
     if (len(signal) < 5*16000):
         print("recording to short")
         print(len(signal))
 
     # add batch and channel dimension
     resized_signal = signal[None, None, :5*16000]
+
+    #make prediction
     with graph.as_default():
         prediction = model.predict(resized_signal)[0]
 
-    # reformat
+    # reformat to percent
     prediction *= 100
+    # round
     prediction = np.round(prediction, decimals=2)
+    # adjust so that sum is 100%
+    prediction[-1] = 100.0 - np.sum(prediction[:-1])
+
     return prediction.tolist()
 
 
@@ -59,16 +67,16 @@ def upload_file():
             # sort predictions
             langs = ["French", "English", "German"]
             s_pred = sorted(zip(pred, langs), reverse=True)
-            # return parsed XML
-            return render_template('prediction.html', l1=str(s_pred[0][1]), p1=str(s_pred[0][0]),
-                                                      l2=str(s_pred[1][1]), p2=str(s_pred[1][0]),
-                                                      l3=str(s_pred[2][1]), p3=str(s_pred[2][0]),)
+            # return parsed XML (round to two decimals)
+            return render_template('prediction.html', l1=s_pred[0][1], p1='%.2f' % s_pred[0][0],
+                                                      l2=s_pred[1][1], p2='%.2f' % s_pred[1][0],
+                                                      l3=s_pred[2][1], p3='%.2f' % s_pred[2][0])
     else:
         # return homepage
         return render_template('site.html')
 
 
-# serve public, https
+# serve publicly
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
             port=8181, debug=True)
