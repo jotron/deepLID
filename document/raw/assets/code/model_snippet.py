93e5d329ce68a883f20032546fb0559381ed68fd
Jotron <jotron@protonmail.com>
2019-01-25 20:29:18 +0100

    fix(docs): new revised version

--- document/raw/assets/code/model_snippet.py
+++ document/raw/assets/code/model_snippet.py
@@ -2,23 +2,25 @@
 from keras import models, layers
 from kapre.time_frequency import Melspectrogram
 
-# Laden von Daten
+# Laden von Daten:
+# DataFeed ist eine selbst geschriebene Klasse zum selektiven Laden von Daten.
 data_path = '../path/to/data'
-train_data, train_labels = DataFeed.Dataset.create(data_path, 
-				['train/voxforge', 'train/youtube'], 
-				num=50000)
-val_data, val_labels = DataFeed.Dataset.create(data_path, 
-				['val/youtube', 'val/voxforge'], 
-				num=10000, shuffle=True)
+
+# Laden von 50'000 Aufnahmen von Voxforge und Youtube
+train_data, train_labels = DataFeed.Dataset.create(data_path,
+                           ['train/voxforge','train/youtube'],
+                           num=50000)
+
+# Laden von 10'000 Aufnahmen von Voxforge und Youtube
+val_data, val_labels = DataFeed.Dataset.create(data_path,
+                       ['val/youtube', 'val/voxforge'],
+                       num=10000, shuffle=True)
 
 # Definieren des Modells
 architecture = [
     Melspectrogram(n_dft=512, input_shape=(1, 5 * 16000,),
                    padding='same', sr=16000, n_mels=28,
-                   fmin=0.0, fmax=10000, power_melgram=1.0,
-                   return_decibel_melgram=False, 
-		   trainable_fb=False,
-                   trainable_kernel=False),
+                   fmin=0.0, fmax=10000),
     layers.Conv2D(64, (3, 6), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 6), activation='relu'),
@@ -30,15 +32,16 @@ architecture = [
     layers.Dense(3, activation='softmax')
 ]
 
+# Zusammenbauen des Modells
 model = models.Sequential(architecture)
 
-# Zusammenbauen des Modells
+# Defninieren des Traininsprozesses: z.b Optimierer, Verlustfunktion
 model.compile(optimizer='Rmsprop',
               metrics=['accuracy'],
               loss='categorical_crossentropy')
 
 # Trainieren des Modells
 model.fit(train_data, train_labels,
-          batch_size=64,
-          epochs=9,
+          batch_size=64, # Mit 64 Eingaben auf einmal tranieren (effizienter)
+          epochs=9, # Neun mal mit allen Daten trainieren
           validation_data=(val_data, val_labels))
